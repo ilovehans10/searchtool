9f75cf6e137b21fc45881e2f99054932fada9c6a
Hans Larsson <hanandlia@gmail.com>
2024-02-08 08:59:47 -0600

    get basic reductive search working and add dependencies to Cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use reductivesearch::reductivesearch;
+use termion::input;
+
 fn main() {
-    println!("Hello, world!");
+    let mut toolsearcher = reductivesearch::Searcher::new(vec![String::from("Hev Suit"), String::from("Portal Gun"), String::from("Grappling Tool"), String::from("Elucidator") ]);
+    toolsearcher.add_search_character('e').expect("should be able to search with an e");
+    dbg!("Searcher: {}", toolsearcher.search_results());
 }
